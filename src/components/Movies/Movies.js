439c757cc0c95a7ab23bc399e8bf5cd6db0e4af8
Kochkin Artem <tomsun86@gmail.com>
2021-05-14 19:06:13 +0300

    feat: Добавлен Preloader

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -4,24 +4,41 @@ import SearchForm from '../SearchForm/SearchForm';
 import MoviesCard from './MoviesCard/MoviesCard';
 import MoviesCardList from './MoviesCardList/MoviesCardList';
 import movieList from '../../utils/Movies.json';
+import Preloader from './Preloader/Preloader';
 
 function Movies() {
   const [currentMovieList, setCurrentMovieList] = React.useState(movieList);
+  const [isLoadingData, setIsLoadingData] = React.useState(true);
   function searchFilter(searchText) {
     setCurrentMovieList(movieList.filter((movie) => movie
       .nameRU.toUpperCase()
       .indexOf(searchText.toUpperCase()) > -1));
   }
+
+  React.useEffect(() => {
+    const loadingDataTimeout = setTimeout(() => {
+      setIsLoadingData(false);
+    }, 1500);
+
+    return () => {
+      clearTimeout(loadingDataTimeout);
+    };
+  }, []);
+
   return (
     <section className="movies">
       <SearchForm
         title="Фильмы"
         search={searchFilter}
       />
-      <MoviesCardList
-        movieList={currentMovieList}
-        component={MoviesCard}
-      />
+      {isLoadingData ? (
+        <Preloader />
+      ) : (
+        <MoviesCardList
+          movieList={currentMovieList}
+          component={MoviesCard}
+        />
+      )}
     </section>
   );
 }

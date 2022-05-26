9b9d9c0221d17963735ed4a30ac5c44b6ddf09eb
Kochkin Artem <tomsun86@gmail.com>
2022-05-27 00:25:00 +0300

    fix: add aria-hidden tag

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -9,7 +9,7 @@ function NotFound() {
     <section className="notfound">
       <h1 className="notfound__title">404</h1>
       <p className="notfound__text">Страница не найдена</p>
-      <p className="noutfound__back" onClick={history.goBack}>Назад</p>
+      <p className="noutfound__back" aria-hidden="true" onClick={history.goBack}>Назад</p>
     </section>
   );
 }

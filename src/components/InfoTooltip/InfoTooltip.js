d38c62c950dbaa7b70c16c00f2627a2ec2ed303b
Kochkin Artem <tomsun86@gmail.com>
2022-05-27 11:17:27 +0300

    fix: use destructuring props assignment

--- src/components/InfoTooltip/InfoTooltip.js
+++ src/components/InfoTooltip/InfoTooltip.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './InfoTooltip.css';
 
-function InfoTooltip(props) {
+function InfoTooltip({ isOpen, name, onClose, errorMessage }) {
   return (
-    <div className={`infotooltip ${props.isOpen && 'infotooltip_opened'}`} id={props.name}>
-      <div onClick={props.onClose} className="infotooltip__overlay" aria-hidden="true" />
+    <div className={`infotooltip ${isOpen && 'infotooltip_opened'}`} id={name}>
+      <div onClick={onClose} className="infotooltip__overlay" aria-hidden="true" />
       <div className="infotooltip__container">
-        <button onClick={props.onClose} className="infotooltip__close" type="button" aria-label="Закрыть" />
-        <h2 className="infotooltip__text">{props.errorMessage}</h2>
+        <button onClick={onClose} className="infotooltip__close" type="button" aria-label="Закрыть" />
+        <h2 className="infotooltip__text">{errorMessage}</h2>
       </div>
     </div>
   );

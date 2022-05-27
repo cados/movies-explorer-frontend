c60c7205fcdd0735359422b20c25b3cf94586a5b
Kochkin Artem <tomsun86@gmail.com>
2022-05-27 11:15:16 +0300

    fix: add aria  tag

--- src/components/InfoTooltip/InfoTooltip.js
+++ src/components/InfoTooltip/InfoTooltip.js
@@ -4,9 +4,9 @@ import './InfoTooltip.css';
 function InfoTooltip(props) {
   return (
     <div className={`infotooltip ${props.isOpen && 'infotooltip_opened'}`} id={props.name}>
-      <div onClick={props.onClose} className="infotooltip__overlay" />
+      <div onClick={props.onClose} className="infotooltip__overlay" aria-hidden="true" />
       <div className="infotooltip__container">
-        <button onClick={props.onClose} className="infotooltip__close" type="button" />
+        <button onClick={props.onClose} className="infotooltip__close" type="button" aria-label="Закрыть" />
         <h2 className="infotooltip__text">{props.errorMessage}</h2>
       </div>
     </div>

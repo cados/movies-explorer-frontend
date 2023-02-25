4e0828e13eeafd3a01b3f4b1aab90975a634352d
Kochkin Artem <tomsun86@gmail.com>
2023-02-25 19:12:18 +0300

    upgrade react to 18 version

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 import './vendor/normalize.css';
 import './index.css';
 import './vendor/fonts/stylesheet.css';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
 );

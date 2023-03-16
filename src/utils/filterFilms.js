6d6847f9739a2353f261d3abcd9f1e06fd53742a
Kochkin Artem <tomsun86@gmail.com>
2023-03-16 13:36:54 +0300

    fix: refactoring

--- src/utils/filterFilms.js
+++ src/utils/filterFilms.js
@@ -1,16 +1,13 @@
-function filterFilms(movies, query, isShortMovie) {
-  return movies.filter((el) => {
-    if (isShortMovie && el.duration > 40) {
-      return false;
-    }
-    // eslint-disable-next-line no-restricted-syntax
-    for (const key of Object.keys(el)) {
-      if (el[key] && el[key].toString().toLowerCase().indexOf(query.toLowerCase()) > -1) {
-        return true;
-      }
+const filterFilms = (movies, query, isShortMovie) => movies.filter((el) => {
+  if (isShortMovie && el.duration > 40) {
+    return false;
+  }
+  return Object.values(el).some((value) => {
+    if (typeof value === 'string') {
+      return value.toLowerCase().includes(query.toLowerCase());
     }
     return false;
   });
-}
+});
 
 export default filterFilms;
